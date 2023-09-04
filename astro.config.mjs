d4e55bf24c85905c0cf3fd66623f577a2f2aad5b
ff6347 <hash@ff6347.email>
2023-09-04 08:23:40 +0200

    feat(init): Setup basic layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,4 @@
 import { defineConfig } from 'astro/config';
 
-import solid from "@astrojs/solid-js";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-
 // https://astro.build/config
-export default defineConfig({
-  integrations: [solid(), tailwind(), sitemap()],
-  server: { port: 8000 }
-});
+export default defineConfig({});
